7e6205f7aac68c2ba0b4000999280abc750b87d7
Susan Cantoni <susan.cantoni1@marist.edu>
2018-01-29 17:49:11 -0500

    Solution to Curiosity rover exercise.

---
+++ rover.py
@@ -0,0 +1,10 @@
+# Introduction to Programming
+# Author: Susan Cantoni
+# Date: 01-29-18
+
+def main ():
+    print("This program illustrates the time it takes a photo to reach NASA from the Mars Curiosity")
+    x= eval(input("Enter number of miles between Mars Curiosity and Earth as x"))
+    time = x / 186000
+    print (time)
+main ()
