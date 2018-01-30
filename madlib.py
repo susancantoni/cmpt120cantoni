84ebae0ee35375232d5591a2e3b2daec70dee511
Susan Cantoni <susan.cantoni1@marist.edu>
2018-01-29 23:30:10 -0500

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,11 @@
+# Introduction to Programming
+# Author: Susan Cantoni
+# Date: 01-29-18
+
+def main ():
+    place = input ("Enter the name of a place.")
+    adjective = input ("Enter an adjective.")
+    animal = input ("Enter a type of animal.")
+    verb = input ("Enter a verb ending in -ing.")
+    print ("On my way to the", place, "I saw a", adjective, animal, verb, "in the middle of the street!")
+main ()
