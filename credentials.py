807fae2b7e50ef88737943f9068e7b2b89f70cab
Susan Cantoni <susan.cantoni1@marist.edu>
2018-02-19 22:29:03 -0500

    Initial program to take first and last name and make a username; ask for password and make sure it's 8 char. then give a Marist email.

---
+++ credentials.py
@@ -0,0 +1,21 @@
+# CMPT 120 Intro to Programming
+# Lab #5 – Working with Strings and Functions
+# Author: Susan Cantoni
+# Created: 2018-02-19
+def main():
+    # get user's first and last names
+    first = input("Enter your first name: ")
+    last = input("Enter your last name: ")
+    # TODO modify this to generate a Marist-style username
+    uname = first + "." + last
+    # ask user to create a new password
+    passwd = input("Create a new password: ")
+    # TODO modify this to ensure the password has at least 8
+    if len(passwd) >= 8:
+        print("Account configured. Your new email address is", uname + "@marist.edu")
+    if len(passwd) < 8:
+        print("Fool of a Took! That password is feeble! It must contain 8 characters!")
+        passwd = input("Create a new password: ")
+        print("The force is strong in this one…")
+        print("Account configured. Your new email address is", uname + "@marist.edu")
+main()
