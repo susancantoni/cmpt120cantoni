624c1eaf90c05c55622cde4bf4e70d7dd79cc60b
Susan Cantoni <susan.cantoni1@marist.edu>
2018-02-22 18:47:57 -0500

    made math fuctions

--- project/calc_functions.py
+++ project/calc_functions.py
@@ -3,5 +3,20 @@
 def add(num1, num2):
     result = num1 + num2
     return result
-#TODO subtract, multiply, change sign
+#TODO subtract, multiply, devide, change sign
 
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
+
+def divide(num1, num2):
+    result = num1 / num2
+    return result
+
+def change_sign(label):
+    result = (label * -1)
+    return result
