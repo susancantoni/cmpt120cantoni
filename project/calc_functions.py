e89e9bbc3d3807da6a15401e6e7843c139e5ff16
Susan Cantoni <susan.cantoni1@marist.edu>
2018-03-06 14:50:55 -0500

    added %, 1 / x, and sqrt

--- project/calc_functions.py
+++ project/calc_functions.py
@@ -4,27 +4,40 @@
 
 
 #calculator math functions
+from math import *
 
 def add(num1, num2):
     result = num1 + num2
-    return result
+    return float (result)
 
 def subtract(num1, num2):
     result = num1 - num2
-    return result
+    return float (result)
 
 def multiply(num1, num2):
     result = num1 * num2
-    return result
+    return float (result)
 
 def divide(num1, num2):
     result = num1 / num2
-    return result
+    return float (result)
 
 def change_sign(num1):
     result = (num1 * -1)
-    return result
+    return float (result)
 
 def square(num1):
     result = num1 * num1
-    return result
+    return float (result)
+
+def over_x(num1):
+    result = 1 / x
+    return float (result)
+
+def percent (num1):
+    result = num1 / 100
+    return float (result)
+
+def square_root (num1):
+    result = sqrt(num1)
+    return float (result)
