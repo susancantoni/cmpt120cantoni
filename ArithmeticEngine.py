438b45095170c4255699ac1dafcbc6685bbad0cc
Susan Cantoni <susan.cantoni1@marist.edu>
2018-03-22 15:30:29 -0400

    initial program, no updates

---
+++ ArithmeticEngine.py
@@ -0,0 +1,32 @@
+# CMPT 120 - Lab #6
+# Susan Cantoni
+# 03-22-2018
+###
+def showIntro():
+    print("Welcome to the Arithmetic Engine!")
+    print("=================================\n")
+    print("Valid commands are 'add', 'mult', 'sub', 'div', and 'quit'.\n")
+def showOutro():
+    print("\nThank you for using the Arithmetic Engine…")
+    rint("\nPlease come back again soon!")
+def doLoop():
+    while True:
+        cmd = input("What computation do you want to perform? ")
+        num1 = int(input("Enter the first number: "))
+        num2 = int(input("Enter the second number: "))
+        if cmd == "add":
+            result = num1 + num2
+        elif cmd == "sub":
+            result = num1 - num2
+        elif cmd == "mult":
+            result = num1 * num2
+        elif cmd == "div":
+            result = num1 // num2
+        elif cmd == "quit":
+            break
+        print("The result is " + str(result) + ".\n")
+def main():
+    showIntro()
+    doLoop()
+    showOutro()
+main()
