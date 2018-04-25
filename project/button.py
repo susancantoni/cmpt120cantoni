b48d78611b53f298c865f66ecccf7304cba1903a
Susan Cantoni <susan.cantoni1@marist.edu>
2018-04-24 22:10:37 -0400

    get rid of activate and deactivate

--- project/button.py
+++ project/button.py
@@ -24,26 +24,12 @@ class Button:
         self.label = Text(center, label)
         self.label.draw(win)
         self.label.setSize(20)
-        #self.deactivate()
 
     def clicked(self, p):
         "Returns true if button active and p is inside"
-        return (self.active and
-                self.xmin <= p.getX() <= self.xmax and
+        return (self.xmin <= p.getX() <= self.xmax and
                 self.ymin <= p.getY() <= self.ymax)
 
     def getLabel(self):
         "Returns the label string of this button."
         return self.label.getText()
-
-    def activate(self):
-        "Sets this button to 'active'."
-        self.label.setFill('black')
-        self.rect.setWidth(2)
-        self.active = True
-
-    def deactivate(self):
-        "Sets this button to 'inactive'."
-        self.label.setFill('darkgrey')
-        self.rect.setWidth(1)
-        self.active = False
